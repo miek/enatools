c0c3b2cedc42b5da6b016d8bc13f3362e3b2c915
Mike Walters <mike@flomp.net>
2022-10-30 12:32:50 +0000

    Add script to save a touchstone file

--- ena.py
+++ ena.py
@@ -1,3 +1,4 @@
+import numpy as np
 import socket
 
 class ENA():
@@ -26,6 +27,30 @@ class ENA():
 
     def set_power(self, power, port=1):
         self.write(f"SOUR{port}:POW:CENT {power}")
+
+    def set_active_trace(self, trace, channel=1):
+        self.write(f"CALC{channel}:PAR{trace}:SEL")
+
+    def get_trace_count(self, channel=1):
+        count = self.query(f"CALC{channel}:PAR:COUNT?")
+        return int(count)
+
+    def get_trace_measurement(self, trace, channel=1):
+        meas = self.query(f"CALC{channel}:PAR{trace}:DEF?")
+        return meas
+
+    def get_freq_data(self, channel=1):
+        data = self.query(f"SENS{channel}:FREQ:DATA?")
+        return np.array([float(x) for x in data.split(',')])
+
+    def get_corrected_data(self, channel=1):
+        data = self.query(f"CALC{channel}:DATA:SDAT?")
+        data = np.array([float(x) for x in data.split(',')])
+        c = np.empty(int(len(data)/2), dtype=np.complex64)
+        c.real = data[0::2]
+        c.imag = data[1::2]
+        return c
+
 
 if __name__ == "__main__":
     dev = ENA()
